0b039b7bac84b373234af1d19a8e43681e0a0f1d
Louis Abramson <pyreta@gmail.com>
2017-08-26 11:26:15 -0500

    style export list message

--- client/src/ExportList.js
+++ client/src/ExportList.js
@@ -4,12 +4,12 @@ import SongList from './SongList';
 
 const ExportList = ({ onDrop, songs, removeSong }) =>
   <div style={{display: 'flex'}}>
-    <Droppable types={['song']} onDrop={onDrop} style={{height: '400px', justifyItems: 'center', flex: '100%'}}>
+    <Droppable types={['song']} onDrop={onDrop} style={{height: '400px', justifyItems: 'center', flex: '100%', flexDirection: 'column', textAlign: songs.length ? 'left' : 'center', paddingTop: songs.length ? '0' : '50%'}}>
       <SongList
         songs={songs}
         songProps={{ deleteSong: removeSong, buttonText: 'Remove' }}
       />
-      {songs.length ? '' : 'Drag songs here to be exported'}
+    <span style={{color: 'grey', fontStyle: 'italic'}}>{songs.length ? '' : 'Drag songs here to be exported'}</span>
     </Droppable>
   </div>
 
